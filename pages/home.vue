f28170dab8414be70d20901a7d043a4ebabf7fef
Kagana1212 <103938608+Kagana1212@users.noreply.github.com>
2023-07-10 05:07:08 +0000

    0710(3)

--- pages/home.vue
+++ pages/home.vue
@@ -14,10 +14,10 @@
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2">
         <div class="card mb-3">
             <div class="card-body">
-            <div class=text-center>
+            <div class="text-center">
             <h3 class="card-title border-bottom border-3 p-2">各種入力</h3>
             <div class="card-text">
-                <>複合仕訳入力</p>
+                <NuxtLink >複合仕訳入力</p>
                 <p>元帳入力</p>
                 <p>通帳入力</p>
             </div>
