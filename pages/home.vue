e4ab55564533278b77bd35496269d568f77a0f4d
Kagana1212 <103938608+Kagana1212@users.noreply.github.com>
2023-07-10 05:50:31 +0000

    0710(4)

--- pages/home.vue
+++ pages/home.vue
@@ -4,35 +4,39 @@
 <div class="row row-cols-md-2 p-3">
     <NuxtLink
     to="./settlement"
-    class="btn btn-primary">年次決算
+    class="btn btn-outline-danger">年次決算
     </NuxtLink>
     <NuxtLink
     to="./settings"
-    class="btn btn-primary">各種設定
+    class="btn btn-outline-danger">各種設定
     </NuxtLink>
 </div>
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2">
-        <div class="card mb-3">
+        <div class="card">
             <div class="card-body">
             <div class="text-center">
             <h3 class="card-title border-bottom border-3 p-2">各種入力</h3>
             <div class="card-text">
-                <NuxtLink >複合仕訳入力</p>
-                <p>元帳入力</p>
-                <p>通帳入力</p>
+            <div class="row row-cols-1">
+                <NuxtLink to="./text" class="btn btn-outline-dark m-2">複合仕訳入力</NuxtLink>
+                <NuxtLink to="./text" class="btn btn-outline-dark m-2">元帳入力</NuxtLink>
+                <NuxtLink to="./text" class="btn btn-outline-dark m-2">通帳入力</NuxtLink>
+            </div>
             </div>
             </div>
             </div>
         </div>
-        <div class="card mb-3">
+        <div class="card">
             <div class="card-body">
             <div class="text-center">
             <h3 class="card-title border-bottom border-3 p-2">各種出力</h3>
             <div class="card-text">
-                <p>仕訳帳</p>
-                <p>出納帳</p>
-                <p>仕訳検索</p>
-                <p>総勘定元帳</p>
+            <div class="row row-cols-1">
+                <NuxtLink to="./text" class="btn btn-outline-dark m-2">仕訳帳</NuxtLink>
+                <NuxtLink to="./text" class="btn btn-outline-dark m-2">出納帳</NuxtLink>
+                <NuxtLink to="./text" class="btn btn-outline-dark m-2">仕訳検索</NuxtLink>
+                <NuxtLink to="./text" class="btn btn-outline-dark m-2">総勘定元帳</NuxtLink>
+            </div>
             </div>
             </div>
             </div>
@@ -40,4 +44,5 @@
     </div>
 </div>
 </template>
+
 
